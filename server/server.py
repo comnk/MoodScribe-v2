9ddcac550481ae95dafe89a942e0db66c79a468c
comnk <vhsiao36@gmail.com>
2024-08-05 17:10:12 -0700

    added back end for text to speech

--- server/server.py
+++ server/server.py
@@ -1,14 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import requests
 import config
+import pyttsx3
 
 import json
 
 app = Flask(__name__)
 CORS(app)
+
+engine = pyttsx3.init()
 
 uri = config.MONGODB_KEY
 client = MongoClient(uri, server_api=ServerApi('1'))
@@ -29,7 +32,22 @@ def get_quote():
     response = requests.get('https://api.quotable.io/random')
     quote = response.json()
     print(quote['content'], quote['author'])
+    #engine.say(quote['content'])
+    #engine.say("By " + quote['author'])
+    #engine.runAndWait() 
     return jsonify({"message": quote['content'], "author": quote['author']})
+
+@app.route('/api/speak', methods=['POST'])
+def text_to_speech():
+    data = request.json
+    text = data.get('message')
+    # Call your text-to-speech function and return the result
+    if (text):
+        engine.say(text)
+        engine.runAndWait()
+        return jsonify({"status": "success", "message": "Text spoken successfully"})
+    else:
+        return jsonify({"status": "error", "message": "No text provided"}), 400
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
